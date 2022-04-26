37b76a14eb056b2955cf2fdfee8c81af5c685daf
stella <mikkamakka89@gmail.com>
2022-04-26 10:30:28 +0200

    day-1

---
+++ frontend/script.js
@@ -0,0 +1,19 @@
+const firstPerson={
+    name: "Peter",
+    sex: "male",
+
+}
+
+const secondPerson = {
+    name: "Sarah",
+    sex: "female"
+}
+
+function detectSex(person){
+    console.log(person)
+    if (person.sex === "female"){
+        return "This person is a female"
+    }else{
+        return 
+    }
+}
